29d5aaa1c451cf3ab563be93744df83f197bef4d
msppyu1 <873634131@qq.com>
2019-04-22 17:55:41 +0800

    digui

--- src/components/detail/Detail.vue
+++ src/components/detail/Detail.vue
@@ -2,6 +2,7 @@
   <div>
    <Banner></Banner>
     <Header></Header>
+    <List :list='list'></List>
     <div class="content"></div>
   </div>
 </template>
@@ -9,15 +10,22 @@
 <script>
 import Banner from './subcomponents/Banner'
 import Header from './subcomponents/Header'
+import List from './subcomponents/List'
 export default {
   name: 'Detail',
   components: {
    Banner,
-   Header
+   Header,
+   List
   },
   data () {
     return {
-     
+     list:[
+     {title:'成人票',children:[{title:'成人三馆联票',children:[{title:'成人三馆联票 - 五一专卖店销售'}]},{title:'成人五馆联票 '}]},
+     {title:'学生票'},
+     {title:'儿童票'},
+     {title:'特惠票'}
+     ]
     }
   },
   
